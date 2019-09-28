cf6ce975a06dd75d8b660e06987c7cdcd2239b37
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2019-09-28 11:08:31 -0500

    passing all six Student model specs

--- db/models/student.js
+++ db/models/student.js
@@ -3,6 +3,37 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('students', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+});
+
+Student.beforeCreate(student => {
+  student.firstName = student.firstName.replace(
+    `${student.firstName[0]}`,
+    `${student.firstName[0].toUpperCase()}`
+  );
+
+  student.lastName = student.lastName.replace(
+    `${student.lastName[0]}`,
+    `${student.lastName[0].toUpperCase()}`
+  );
+});
+
+Student.create();
 
 module.exports = Student;
